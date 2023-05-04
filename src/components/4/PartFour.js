8f04337c8dcd33875d5e78d93bacf98d0b5e3da7
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-04 16:05:15 +0100

    object state variable

--- src/components/4/PartFour.js
+++ src/components/4/PartFour.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./partFour.css";
 
 const PartFour = () => {
-  return <div>PartFour</div>;
+  const [state, setState] = useState({
+    department: "Biochemistry",
+    level: 100,
+    year: 2019,
+  });
+  const handleClick = (val) =>
+    setState({
+      ...state,
+      level: state[val] + 100,
+      year: state[val] + 1,
+    });
+  const { department, level, year } = state;
+  return (
+    <div>
+      <h1>Object State Variables</h1>
+      <h3>
+        I'm a {level} level {department} student in {year}
+      </h3>
+      <div>
+        <button onClick={handleClick.bind(null, "level")}>Increment</button>
+      </div>
+    </div>
+  );
 };
 
 export default PartFour;
