18b9458eb94b0f5e8845725dc5b0c9b5768eff85
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2023-05-12 22:43:37 +0100

    part 4: updating

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const App = () => {
       <PartThree />
       <hr />
       <PartFour />
+      <hr />
     </div>
   );
 };
---
+++ src/components/5/PartFive.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./partFive.css";
+
+const PartFive = () => {
+  return <div>PartFive</div>;
+};
+
+export default PartFive;
